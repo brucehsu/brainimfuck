295c4542dc3831e0a71ebb6a293db6760cee3fd7
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-16 16:29:40 +0800

    Extract VM struct to individual module

--- brainimfuck.nim
+++ brainimfuck.nim
@@ -1,21 +1,4 @@
-type VM* = object
-  mem : seq[int]
-  idx : int
-
-proc addMem(vm: var VM): void = 
-  vm.mem[vm.idx] += 1
-proc subMem(vm: var VM): void = 
-  vm.mem[vm.idx] -= 1
-proc movePtr(vm: var VM, offset: int): int=
-  if vm.idx + offset >= vm.mem.len():
-    for i in countup(0, ((vm.idx + offset + 1) - vm.mem.len())):
-      vm.mem.add(0)
-  elif vm.idx + offset < 0:
-    return -1 # Should raise error
-  vm.idx += offset
-proc printMem(vm: var VM): void =
-  echo char(vm.mem[vm.idx])
-
+import bfvm
 var
   insts : string
   vm = VM(mem: newSeq[int](1))
