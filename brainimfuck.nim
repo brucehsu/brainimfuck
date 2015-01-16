9c30dfd8744bdaca71f6b8d6e6fe8da5f22aca06
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-16 16:45:00 +0800

    Read entire instructions in before execution

--- brainimfuck.nim
+++ brainimfuck.nim
@@ -1,31 +1,33 @@
 import bfvm
 var
-  insts : string
+  insts : string = ""
   vm = VM(mem: newSeq[int](1))
 
-block root:
-  while true:
-    try:
-      insts = readLine(stdin)
-    except IOError:
-      break
+while true:
+  try:
+    let inst = readLine(stdin)
+    insts.add(inst)
+  except IOError:
+    break
 
-    for cmd in insts:
-      case cmd:
-        of '+':
-          vm.addMem()
-        of '-':
-          vm.subMem()
-        of '<':
-          if vm.movePtr(-1) == -1:
-            echo "Runtime error"
-            break root
-        of '>':
-          if vm.movePtr(1) == -1:
-            echo "Runtime error"
-            break root
-        of '.':
-          vm.printMem()
-        else:
-          echo "Syntax error"
-          break root
+echo insts
+
+for cmd in insts:
+  case cmd:
+    of '+':
+      vm.addMem()
+    of '-':
+      vm.subMem()
+    of '<':
+      if vm.movePtr(-1) == -1:
+        echo "Runtime error"
+        break
+    of '>':
+      if vm.movePtr(1) == -1:
+        echo "Runtime error"
+        break
+    of '.':
+      vm.printMem()
+    else:
+      echo "Syntax error"
+      break
